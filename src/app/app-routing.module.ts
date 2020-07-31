db3d53892e01d9364df5652faa69485e061ab71f
Camilo Ramos <cramoss91@gmail.com>
2020-07-31 15:50:03 -0400

    Added register-user form.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {MainLoginComponent} from './login/components/main-login/main-login.component';
-import {MainHomeComponent} from './home/components/main-home/main-home.component';
+import { MainLoginComponent } from './login/components/main-login/main-login.component';
+import { MainHomeComponent } from './home/components/main-home/main-home.component';
+import {MainRegisterComponent} from './register-user/components/main-register/main-register.component';
 
 const routes: Routes = [
   { path: 'login', component: MainLoginComponent },
   { path: 'home', component: MainHomeComponent },
+  { path: 'newuser', component: MainRegisterComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login', pathMatch: 'full' }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './global/header/header.component';
 import { FooterComponent } from './global/footer/footer.component';
 import { LoginModule } from './login/login.module';
+import { RegisterUserModule } from './register-user/register-user.module';
 
 @NgModule({
   declarations: [
@@ -18,7 +18,8 @@ import { LoginModule } from './login/login.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    LoginModule
+    LoginModule,
+    RegisterUserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
